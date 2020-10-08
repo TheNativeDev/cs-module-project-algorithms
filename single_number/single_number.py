e62eaafdaaabd0d30f58f61e17efef5c81993947
TheNativeDev <thenativedev@outlook.com>
2020-10-08 04:12:05 -0700

    working on mvp

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,14 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
-
-    pass
+    result = 0
+    for i in arr:
+        result ^= i
+        return result
 
 
 if __name__ == '__main__':
